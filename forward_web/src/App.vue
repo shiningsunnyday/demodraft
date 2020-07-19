df33318e8f3932af00e4dead07f052ce23158544
Michael Sun <michaelsun18@yahoo.com>
2020-07-19 13:49:08 -0700

    first forward web commit

---
+++ forward_web/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/once_again.jpg" width="30%" height="30%">
+    <h1>{{msg}}</h1>
+    <button type = "button" id = "get-politicians" @click = "fetchInit">Politicians</button>
+    <ul id="politician-list" style="list-style: none;">
+      <li v-for="p in res" :key="p.name">
+        <p style="text-align: left;">{{p.name}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+        msg: "Humanity Forward",
+        res: [],
+        avail: false
+    }
+  },
+  methods: {
+        fetchInit() {
+            fetch("http://ec2-18-144-155-31.us-west-1.compute.amazonaws.com/politicians", {
+                "method": "GET",
+            }).then(res => {
+                if(res.ok){
+                    return res.json()
+                }
+            }).then(res => {
+                this.res = res;
+                this.avail = true;
+                console.log(res);
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ forward_web/src/components/Democrats.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="democrat-list">
+    <button type = "button" id = "get-politicians" @click = "fetchInit">Politicians</button>
+    <div v-if = "avail == true"><i>{{res}}</i></div>
+    <p>Hi</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemocratList',
+  data: {
+    'avail': false,
+    'res': false
+  },
+  methods: {
+
+  }
+}
+</script>
