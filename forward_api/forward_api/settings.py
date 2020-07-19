7e4690c4dd406685b62421adbfc8376eaf457642
Michael Sun <michaelsun18@yahoo.com>
2020-07-19 09:29:35 -0700

    CORS

--- forward_api/forward_api/settings.py
+++ forward_api/forward_api/settings.py
@@ -27,6 +27,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    'GET'
+]
+
 
 # Application definition
 
@@ -38,12 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forward_app',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
