aa8fd43bb73e763e2593da299f50e18596bb7aad
Alex Li <sf.alexli@gmail.com>
2020-07-26 21:01:02 -0700

    add eslint config & format files

--- forward_web/src/App.vue
+++ forward_web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/once_again.jpg" width="30%" height="100%">
+    <img alt="Vue logo" src="./assets/once_again.jpg" width="30%" height="100%" />
     <h1>{{msg}}</h1>
     <button type="button" id="get-politicians" @click="fetchInit">Politicians</button>
     <ul class="politician-list" id="politician-list">
@@ -17,32 +17,32 @@ export default {
   name: 'App',
   data() {
     return {
-        msg: "Humanity Forward!!!",
-        res: [],
-        avail: false
-    }
+      msg: "Humanity Forward!!!",
+      res: [],
+      avail: false
+    };
   },
   methods: {
-        fetchInit() {
-            fetch("http://ec2-18-144-155-31.us-west-1.compute.amazonaws.com/politicians", {
-                "method": "GET",
-            }).then(res => {
-                if(res.ok){
-                    return res.json()
-                }
-            }).then(res => {
-                this.res = res;
-                this.avail = true;
-                console.log(res);
-            }).catch(err => {
-                console.log(err);
-            });
+    fetchInit() {
+      fetch("http://ec2-18-144-155-31.us-west-1.compute.amazonaws.com/politicians", {
+        "method": "GET",
+      }).then(res => {
+        if(res.ok){
+          return res.json();
         }
-    },
+      }).then(res => {
+        this.res = res;
+        this.avail = true;
+        console.log(res);
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  },
   components: {
 
   }
-}
+};
 </script>
 
 <style>
