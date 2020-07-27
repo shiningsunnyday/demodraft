fd2635a3a55996bc5d45f8931dd8c9d40d3d148e
Michael Sun <michaelsun18@yahoo.com>
2020-07-27 15:25:01 -0700

    add tests module

---
+++ forward_api/forward_app/tests/test_policy.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+"""
+Tests for all functionality with Policy object
+"""
+class PolicyTest(TestCase):
+    raise NotImplementedError
---
+++ forward_api/forward_app/tests/test_politician.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+"""
+Tests for all functionality with Politician object
+"""
+class PoliticianTest(TestCase):
+    raise NotImplementedError
