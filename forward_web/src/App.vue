4c277ce92c1f447f0e0bb998ff641f641908feeb
Jaytee <Padilla.Jaytee@gmail.com>
2020-07-27 20:10:46 -0700

    Merged web_dev branch updates to my branch

--- forward_web/src/App.vue
+++ forward_web/src/App.vue
@@ -1,47 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/once_again.jpg" width="30%" height="100%" />
-    <h1>{{msg}}</h1>
-    <button type="button" id="get-politicians" @click="fetchInit">Politicians</button>
+    <img
+      alt="Vue logo"
+      src="./assets/once_again.jpg"
+      width="30%"
+      height="100%"
+    />
+    <h1>{{ msg }}</h1>
+    <button type="button" id="get-politicians" @click="fetchInit">
+      Politicians
+    </button>
     <ul class="politician-list" id="politician-list">
       <li v-for="p in res" :key="p.name">
-        <p>{{p.name}}</p>
+        <p>{{ p.name }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       msg: "Humanity Forward!!!",
       res: [],
-      avail: false
+      avail: false,
     };
   },
   methods: {
     fetchInit() {
-      fetch("http://ec2-18-144-155-31.us-west-1.compute.amazonaws.com/politicians", {
-        "method": "GET",
-      }).then(res => {
-        if(res.ok){
-          return res.json();
+      fetch(
+        "http://ec2-18-144-155-31.us-west-1.compute.amazonaws.com/politicians",
+        {
+          method: "GET",
         }
-      }).then(res => {
-        this.res = res;
-        this.avail = true;
-        console.log(res);
-      }).catch(err => {
-        console.log(err);
-      });
-    }
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((res) => {
+          this.res = res;
+          this.avail = true;
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
-  components: {
-
-  }
+  components: {},
 };
 </script>
 
