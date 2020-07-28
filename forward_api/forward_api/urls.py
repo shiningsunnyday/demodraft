07af17f9ab27b14552edc262fbac470bf991a36b
Michael Sun <michaelsun18@yahoo.com>
2020-07-27 23:29:32 -0700

    default router

--- forward_api/forward_api/urls.py
+++ forward_api/forward_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from forward_app import views
 
-# router = routers.DefaultRouter()
+router = routers.DefaultRouter()
 # router.register('signup', views.SignupViewSet, basename="signup")
 # router.register('groups', views.GroupViewSet)
 
@@ -10,7 +10,7 @@ from forward_app import views
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
     path('signup/', views.Signup.as_view(), name="signup"),
     path('login/', views.Login.as_view(), name="login"),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
