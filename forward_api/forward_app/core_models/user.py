ba6a6ad6296dbdb652bce70eff29645d33af6369
Brian Li <bl195@duke.edu>
2020-07-27 21:30:14 -0400

    added user and persona models

--- forward_api/forward_app/core_models/user.py
+++ forward_api/forward_app/core_models/user.py
@@ -1,10 +1,21 @@
 from django.db import models
 
-# class User(models.Model):
-#     class Meta:
-#
+class User(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=True, default='')
+    email = models.CharField(max_length=100, blank=True, default='')
+    address = CharField(max_length=100, blank=True, default='')
+    class Meta:
+        ordering = ['created']
+
 # """
 # One-to-one relation w/ User
 # """
-# class Persona(models.Model):
-#     class Meta:
+class Persona(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+    created = models.DateTimeField(auto_now_add=True)
+    followers = models.IntegerField(default=0)
+    status = models.CharField(max_length=100, blank=True, default='')
+    class Meta:
+        ordering = ['created']
