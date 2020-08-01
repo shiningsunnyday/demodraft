e1027193d807e9af44ee5df7e93c1493f417fd42
Alex Li <sf.alexli@gmail.com>
2020-08-01 00:16:58 -0700

    Added auth to policy pages

--- forward_web/src/stores/store.js
+++ forward_web/src/stores/store.js
@@ -35,19 +35,18 @@ export const store = new Vuex.Store({
         //////
         ////  test data
         // 
-        const token = 'token321';
         const data = {
-          token: token,
-          user: {
-            username: 'test',
-            password: 'test'
-          }
+          token: 'token321',
+          user: user
         };
         
-        localStorage.setItem('token', token);
-        axios.defaults.headers.common['Authorization'] = token;
+        localStorage.setItem('token', data.token);
+        axios.defaults.headers.common['Authorization'] = data.token;
+        console.log('Login: ', axios.defaults.headers.common['Authorization']);
+        console.log(data.user);
         commit('auth_success', data);
-        resolve(user);
+        resolve();
+
         //
         /// end test data
         ////
@@ -80,40 +79,53 @@ export const store = new Vuex.Store({
     register({ commit }, user) {
       return new Promise((resolve, reject) => {
         commit('auth_request');
-        axios({
-          url: 'http://ec2-54-183-146-26.us-west-1.compute.amazonaws.com/signup/',
-          data: user,
-          method: 'POST',
-          headers: {'content-type': 'application/json'},
-          auth: {
-            username: 'admin',
-            password: 'password'
-          }
-        })
-          .then((resp) => {
-            //const token = resp.data.token;
-            const token = 'token123';
-            const data = {
-              token: 'token123', 
-              user: resp.data
-            };
-            localStorage.setItem('token', token);
-            axios.defaults.headers.common['Authorization'] = token;
-            commit('auth_success', data);
-            console.log(resp);
-            resolve(resp);
-          })
-          .catch((err) => {
-            commit('auth_error', err);
-            localStorage.removeItem('token');
-            reject(err);
-          });
+
+        const data = {
+          token: token321,
+          user: user
+        };
+        
+        localStorage.setItem('token', data.token);
+        axios.defaults.headers.common['Authorization'] = data.token;
+        console.log('Register: ', axios.defaults.headers.common['Authorization']);
+        commit('auth_success', data);
+        resolve();
+
+      //   axios({
+      //     url: 'http://ec2-54-183-146-26.us-west-1.compute.amazonaws.com/signup/',
+      //     data: user,
+      //     method: 'POST',
+      //     headers: {'content-type': 'application/json'},
+      //     auth: {
+      //       username: 'admin',
+      //       password: 'password'
+      //     }
+      //   })
+      //     .then((resp) => {
+      //       console.log('token', resp.data);
+      //       const token = 'token123';
+      //       const data = {
+      //         token: 'token123', 
+      //         user: resp.data
+      //       };
+      //       localStorage.setItem('token', token);
+      //       axios.defaults.headers.common['Authorization'] = data;
+      //       commit('auth_success', data);
+      //       resolve(resp);
+      //     })
+      //     .catch((err) => {
+      //       commit('auth_error', err);
+      //       localStorage.removeItem('token');
+      //       reject(err);
+      //     });
+      // });
       });
     },
     logout({ commit }) {
       return new Promise((resolve, reject) => {
         commit('logout');
         localStorage.removeItem('token');
+        console.log('Logout: ', axios.defaults.headers.common['Authorization']);
         delete axios.defaults.headers.common['Authorization'];
         resolve();
       });
