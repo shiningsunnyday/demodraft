ba240a1f15937312edda66b19399a17381a37280
Alex Li <sf.alexli@gmail.com>
2020-08-01 19:11:23 -0700

    Updated policy cards and navbar views

--- forward_web/src/components/PolicyList.vue
+++ forward_web/src/components/PolicyList.vue
@@ -5,37 +5,51 @@
       v-for="policy in filteredPolicies"
       :key="policy.id"
     >
-      <router-link
-        class="policy__route"
-        v-bind:to="{
-          name: 'policy-page',
-          params: {
-            id: policy.id
-          },
-        }"
-      >
-        <BCard v-bind:title="policy.name" class="mb-2 policy__card">
-          <b-card-text>
-            Category: {{ policy.category }}
-          </b-card-text>
-          <b-card-text>
-            {{ policy.title }}
-          </b-card-text>
-          <BButton>Go somewhere</BButton>
-        </BCard>
-      </router-link>
+      <!-- Move this into own component later -->
+      <BCard class="mb-2 policy__card">
+        <b-card-title>
+          <b-button
+            v-b-toggle
+            :href="`#collapse-${policy.id}`"
+            @click.prevent
+          >
+            {{ policy.name }}
+          </b-button>
+        </b-card-title>
+        <b-collapse :id="`collapse-${policy.id}`" class="mt-2">
+          <BCard>
+            <p class="card-text">{{ policy.statement }}</p>
+            <BButton variant="link">
+              <router-link
+                class="policy__route"
+                v-bind:to="{
+                  name: 'policy-page',
+                  params: {
+                    id: policy.id,
+                  },
+                }"
+              >
+                Learn more
+              </router-link>
+            </BButton>
+          </BCard>
+        </b-collapse>
+        <b-card-text class="policy__category"> 
+          Category: {{ policy.category }} 
+        </b-card-text>
+      </BCard>
     </div>
   </div>
 </template>
 
 <script>
-import { BCard, BButton } from "bootstrap-vue";
+import { BCard, BButton } from 'bootstrap-vue';
 
 export default {
-  name: "PolicyList",
+  name: 'PolicyList',
   components: {
-    "b-button": BButton,
-    "b-card": BCard,
+    'b-button': BButton,
+    'b-card': BCard,
   },
   props: {
     filteredPolicies: {
@@ -58,13 +72,16 @@ export default {
 
   &__card {
     width: 300px;
-    height: 300px;
-    cursor: pointer;
+    //cursor: pointer;
   }
 
   &__route {
     text-decoration: none;
     color: unset;
+  }
+
+  &__category {
+    margin-top: 1em;
   }
 }
 </style>
