124ab8e932b9c0fafd124d9af04fb5f07d918c7e
Jaytee Padilla <34228578+jaytee-padilla@users.noreply.github.com>
2020-08-01 19:38:01 -0700

    Rendered comments from API in CommentList (#25)
    Comments from API are now rendering in CommentList

--- forward_web/src/components/CommentList.vue
+++ forward_web/src/components/CommentList.vue
@@ -3,18 +3,18 @@
     <h1>CommentList Component</h1>
 
     <div v-if="comments.length" class="comments-wrapper">
-      <div v-for="comment in comments" :key="comment.id">
-        <ul>
-          <li>{{ comment }}</li>
-        </ul>
+      <div v-for="comment in comments" :key="comment.id" class="comments-wrapper__comment">
+        <p>{{ comment.username }}</p>
+        <p>{{ comment.content }}</p>
+        <p>Likes: {{ comment.likes }}</p>
+        <BButton>Like dis comment</BButton>
       </div>
     </div>
-
-    <button>View Comments</button>
   </div>
 </template>
 
 <script>
+import { BButton } from "bootstrap-vue";
 import { ApiUtil } from "../_utils/api-utils";
 // 1. Get list of comments linked to selected PolicyPage's id
 // 2. For each comment that exists, created a CommentCard component
@@ -27,20 +27,33 @@ export default {
       comments: [],
     };
   },
+  components: {
+    "b-button": BButton,
+  },
   methods: {},
   async created() {
     // *** Squashing all policy comments into a single array because it's easier to work with at the moment (need help understanding thread data structure better) ***
-    const policyComments = await ApiUtil.getPolicyComments(this.$route.params.id);
+    const policyComments = await ApiUtil.getPolicyComments(
+      this.$route.params.id
+    );
 
     policyComments.forEach((currArr) => {
       currArr.forEach((currComm) => {
         this.comments.push(currComm);
       });
     });
-    
-    console.log(this.comments);
+
+    // console.log(this.comments);
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.comments-wrapper {
+  &__comment {
+    border: 1px solid black;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+}
+</style>
