ac9c0528968fa650620449c907330e3719f2b146
Alex Li <sf.alexli@gmail.com>
2020-08-02 00:26:18 -0700

    Added working comments and reply functionality

--- forward_web/src/components/CommentList.vue
+++ forward_web/src/components/CommentList.vue
@@ -2,58 +2,102 @@
   <div>
     <h1>CommentList Component</h1>
 
-    <div v-if="comments.length" class="comments-wrapper">
-      <div v-for="comment in comments" :key="comment.id" class="comments-wrapper__comment">
-        <p>{{ comment.username }}</p>
-        <p>{{ comment.content }}</p>
-        <p>Likes: {{ comment.likes }}</p>
-        <BButton>Like dis comment</BButton>
-      </div>
+    <div 
+      class="comments-wrapper"
+      v-if="allComments.length" 
+      v-for="(comment, index) in allComments" 
+      :key="`comment-${index}`"
+    >
+      <CommentThread :comment="comment"></CommentThread>
     </div>
   </div>
 </template>
 
 <script>
-import { BButton } from "bootstrap-vue";
+import { BButton, BIcon, BIconHandThumbsUp } from "bootstrap-vue";
 import { ApiUtil } from "../_utils/api-utils";
+import CommentThread from './CommentThread';
 // 1. Get list of comments linked to selected PolicyPage's id
 // 2. For each comment that exists, created a CommentCard component
 // 3. Render each of those CommentCard components
 
 export default {
   name: "CommentList",
+  /** 
+   * lets keep components at the top - underneath name:
+   * so we know what components are being used right away
+  */
+  components: {
+    "b-button": BButton,
+    BIcon,
+    BIconHandThumbsUp,
+    CommentThread,
+  },
   data() {
     return {
       comments: [],
+      allComments:[],
+      isViewReplies: false,
     };
   },
-  components: {
-    "b-button": BButton,
-  },
-  methods: {},
+  /**
+   * lets move life cycles above methods
+   * to more represent the order of events on component creation
+   */
   async created() {
+    const { comments, allComments } = this;
     // *** Squashing all policy comments into a single array because it's easier to work with at the moment (need help understanding thread data structure better) ***
     const policyComments = await ApiUtil.getPolicyComments(
       this.$route.params.id
     );
 
+    /**
+     * Very convoluted way of storing comments at the moment.
+     * Will need to see if there is a more efficient way of 
+     * structuring leading comments and replies to those leading comments
+     * 
+     * for now this would look like:
+     * [
+     *  [{leading comment}, [array of replies to leading comment]]
+     *  ie: [{}, [{},{},{},{}}]
+     * ]
+     */
     policyComments.forEach((currArr) => {
-      currArr.forEach((currComm) => {
-        this.comments.push(currComm);
+      let thread = [];
+      let subComments = [];
+      currArr.forEach((currComm, index) => {
+        if (index === 0) {
+          thread.push(currComm);
+          return;
+        } 
+
+        if (currComm.id === currComm.next_comment_id) {
+          subComments.push(currComm);
+          thread.push(subComments);
+          return;
+        }
+
+        subComments.push(currComm);
       });
+      
+      allComments.push(thread);
     });
 
-    // console.log(this.comments);
+    //console.log(allComments);
+  },
+  methods: {
+    handleViewReplies() {
+      this.isViewReplies = !this.isViewReplies;
+      console.log("clicked");
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .comments-wrapper {
-  &__comment {
-    border: 1px solid black;
-    padding: 10px;
-    margin-bottom: 10px;
-  }
+  text-align: start;
+  max-width: 700px;
+  margin: 0 auto;
 }
 </style>
