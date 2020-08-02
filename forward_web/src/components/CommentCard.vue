b93975cb4184889620bcdbc1dbf98f5021de03b1
Alex Li <sf.alexli@gmail.com>
2020-08-02 15:15:00 -0700

    Updated likes to be reactive

---
+++ forward_web/src/components/CommentCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="comments-wrapper__comment" :class="className">
+    <p class="comments-wrapper__username">{{ comment.username }}</p>
+    <p class="comments-wrapper__content">{{ comment.content }}</p>
+    <div class="comments-wrapper__like">
+      <BIconHandThumbsUp
+        class="comments-wrapper__like-icon"
+        variant="dark"
+        v-on:click="handleClick(comment.id)"
+      />
+      <span v-if="!liked">{{ comment.likes }}</span>
+      <span v-else>{{ likes }}</span>
+      <slot></slot>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import { BIcon, BIconHandThumbsUp } from 'bootstrap-vue';
+import { ApiUtil } from '../_utils/api-utils';
+
+export default {
+  name: 'CommentCard',
+  components: {
+    BIcon,
+    BIconHandThumbsUp,
+  },
+  props: {
+    comment: Object,
+    className: String,
+  },
+  data() {
+    return {
+      liked: false,
+      likes: 0,
+      isViewReplies: false,
+      alreadyViewed: false,
+    };
+  },
+  methods: {
+    // prevent hard spamming likes for now
+    // will need to use Vuex state managment to handle this
+    // specific user since you can refresh the page and like again
+    async handleClick(id) {
+      if (!this.liked) {
+        this.likes = await ApiUtil.commentLike(id);
+        this.liked = true;
+      } 
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- forward_web/src/components/CommentThread.vue
+++ forward_web/src/components/CommentThread.vue
@@ -1,46 +1,32 @@
 <template>
-  <!-- Leading comment -->
-  <div v-bind:comment="comment" class="comments-wrapper__comment comments-wrapper__lead-comment">
-    <p class="comments-wrapper__username">{{ comment[0].username }}</p>
-    <p class="comments-wrapper__content">{{ comment[0].content }}</p>
-    
-    <div class="comments-wrapper__like">
-      <button v-on:click="handleClick(0)">
-        <BIconHandThumbsUp class="comments-wrapper__like-icon" variant="dark" />
-        <span>{{ comment[0].likes }}</span>
-      </button>
-      
-      <BButton 
+  <div class="comments-wrapper__comment">
+    <!-- Leading comment -->
+    <CommentCard
+      :comment="comment[0]"
+      :className="`comments-wrapper__lead-comment`"
+    >
+      <BButton
         v-if="comment[1]"
-        variant="link" 
-        @click="handleViewReplies"
+        variant="link"
+        @click="handleViewReplies(comment[0].id)"
       >
         view replies
       </BButton>
-    </div>
+    </CommentCard>
 
-    
     <!-- replies to leading comment -->
-    <div v-if="isViewReplies" v-for="replies in comment[1]" :key="replies.id">
-      <div class="comments-wrapper__comment comments-wrapper__sub-comment">
-        <p class="comments-wrapper__username">{{ replies.username }}</p>
-        <p class="comments-wrapper__content">{{ replies.content }}</p>
-        <button v-on:click="handleClick(replies.id)">
-          <div class="comments-wrapper__like">
-            <BIconHandThumbsUp
-              class="comments-wrapper__like-icon"
-              variant="dark"
-            />
-            <span>{{ replies.likes }}</span>
-          </div>
-        </button>
-      </div>
+    <div v-if="isViewReplies" v-for="replies in thread" :key="replies.id">
+      <CommentCard
+        :comment="replies"
+        :className="`comments-wrapper__sub-comment `"
+      ></CommentCard>
     </div>
   </div>
 </template>
 
 <script>
 import { BButton, BIcon, BIconHandThumbsUp } from 'bootstrap-vue';
+import CommentCard from './CommentCard';
 import { ApiUtil } from '../_utils/api-utils';
 
 export default {
@@ -49,29 +35,32 @@ export default {
     'b-button': BButton,
     BIcon,
     BIconHandThumbsUp,
-  },
-  data() {
-    return {
-      isViewReplies: false,
-    };
+    CommentCard,
   },
   props: {
     comment: Array,
     className: String,
-    isLeadComment: true,
+  },
+  data() {
+    return {
+      isViewReplies: false,
+      thread: [],
+    };
   },
   methods: {
-    handleViewReplies() {
+    async handleViewReplies(leadCommentId) {
+      // prevent unecessary api calls when closing replies
+      if (!this.isViewReplies) {
+        console.log('fetch thread');
+        this.thread = await ApiUtil.getThreadFromComment(leadCommentId);
+      }
       this.isViewReplies = !this.isViewReplies;
     },
-    async handleClick(id) {
-      let likes = await ApiUtil.commentLike(id);
-    }
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .comments-wrapper {
   text-align: start;
   max-width: 700px;
@@ -101,6 +90,10 @@ export default {
 
   &__like-icon {
     margin: 8px;
+    cursor: pointer;
+    &:hover {
+      fill: green;
+    }
   }
 
   .btn {
