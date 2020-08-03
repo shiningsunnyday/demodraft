de70f915f8ade618093a95ef65629ada320592a0
Michael Sun <michaelsun18@yahoo.com>
2020-08-02 20:13:33 -0700

    Added 'like' functionality and updated comment fetching (#27)
    * like policy triggers put
    
    * put now works
    
    * comment like, reply like but doesn't trigger re-render cause comment is prop
    
    * Updated likes to be reactive (#28)
    
    * Updated likes to be reactive
    
    * Removed unused data
    
    * Fixed thread fetching to reflect server updates (#29)
    
    * Rebased alex/update-likes
    
    * Added temp fix to prevent spamming of likes
    
    Co-authored-by: Alex Li <sf.alexli@gmail.com>

--- forward_web/src/_utils/api-utils.js
+++ forward_web/src/_utils/api-utils.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 export class ApiUtil {
+
+  static api_url = "http://ec2-54-183-146-26.us-west-1.compute.amazonaws.com";
+
   static async getPolicies() {
     let response;
 
     try {
-      response = await axios.get("http://ec2-54-183-146-26.us-west-1.compute.amazonaws.com/policies/");
+      response = await axios.get(`${ApiUtil.api_url}/policies/`);
     } catch (error) {
       console.error(error.message);
     }
@@ -16,7 +19,7 @@ export class ApiUtil {
   static async getPolicy(id) {
     let response;
     try {
-      response = await axios.get(`http://ec2-54-183-146-26.us-west-1.compute.amazonaws.com/policy/?id=${id}`);
+      response = await axios.get(`${ApiUtil.api_url}/policy/?id=${id}`);
     } catch (error) {
       console.error(error.message);
     }
@@ -27,11 +30,45 @@ export class ApiUtil {
   static async getPolicyComments(id) {
     let response;
     try {
-      response = await axios.get(`http://ec2-54-183-146-26.us-west-1.compute.amazonaws.com/thread/?policy_id=${id}`);
+      response = await axios.get(`${ApiUtil.api_url}/thread/?policy_id=${id}`);
     } catch (error) {
       console.error(error.message);
     }
 
     return response.data;
   }
+
+  static async getThreadFromComment(id) {
+    let response;
+    try {
+      response = await axios.get(`${ApiUtil.api_url}/thread/?thread_id=${id}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    return response.data.splice(1);
+  }
+
+  static async policyLike(id) {
+    let response;
+    try {
+      response = await axios.put(`${ApiUtil.api_url}/policy/`, {id: id});
+    } catch (error) {
+      console.error(error.message);
+    }
+    return response.data.likes;
+  }
+
+  static async commentLike(id) {
+    let response;
+    try {
+      response = await axios.patch(`${ApiUtil.api_url}/comment/`, {comment_id: id});
+    } catch (error) {
+      console.error(error.message);
+    }
+    console.log(response.data);
+    
+    return response.data.likes;
+  }
 }
+
---
+++ forward_web/src/components/comments/CommentThread.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="comments-wrapper__comment">
+    <!-- Leading comment -->
+    <CommentCard
+      :comment="comment"
+      :className="`comments-wrapper__lead-comment`"
+    >
+      <BButton
+        v-if="hasReplies"
+        @click="handleViewReplies(comment.thread_id)"
+        variant="link"
+      >
+        view replies
+      </BButton>
+    </CommentCard>
+
+    <!-- replies to leading comment -->
+    <div v-if="isViewReplies" v-for="replies in thread" :key="replies.id">
+      <CommentCard
+        :comment="replies"
+        :className="`comments-wrapper__sub-comment `"
+      ></CommentCard>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BButton, BIcon, BIconHandThumbsUp } from 'bootstrap-vue';
+import CommentCard from './CommentCard';
+import { ApiUtil } from "@/_utils/api-utils.js";
+
+export default {
+  name: 'CommentThread',
+  components: {
+    'b-button': BButton,
+    BIcon,
+    BIconHandThumbsUp,
+    CommentCard,
+  },
+  props: {
+    comment: Object,
+    className: String,
+  },
+  data() {
+    return {
+      isViewReplies: false,
+      hasReplies: false,
+      thread: [],
+    };
+  },
+  async mounted () {
+    const { comment } = this; // props
+    this.thread = await ApiUtil.getThreadFromComment(comment.thread_id);
+    this.hasReplies = this.thread.length > 0 ? true : false;
+  },
+  methods: {
+    async handleViewReplies(threadId) {
+      // prevent unecessary api calls when closing replies
+      if (!this.isViewReplies) {
+        this.thread = await ApiUtil.getThreadFromComment(threadId);
+        //console.log('thread fetched');
+      }
+      this.isViewReplies = !this.isViewReplies;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.comments-wrapper {
+  text-align: start;
+  max-width: 700px;
+  margin: 0 auto;
+  &__comment {
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+
+  &__lead-comment {
+    border: 1px solid rgb(224, 224, 224);
+  }
+
+  &__sub-comment {
+    border: 1px solid rgb(224, 224, 224);
+    margin-left: 2rem;
+  }
+
+  &__child-comment {
+    margin-left: 8px;
+  }
+
+  &__like {
+    display: flex;
+    align-items: center;
+  }
+
+  &__like-icon {
+    margin: 8px;
+    cursor: pointer;
+    &:hover {
+      fill: green;
+    }
+  }
+
+  .btn {
+    margin: 8px;
+  }
+}
+</style>
