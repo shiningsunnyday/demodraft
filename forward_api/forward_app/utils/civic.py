70df4c41fec2e4406d951116f8afb5af4fa92f21
Michael Sun <michaelsun18@yahoo.com>
2020-08-03 00:40:23 -0700

    refactor views and address POST endpoint which returns representatives (#32)

---
+++ forward_api/forward_app/utils/civic.py
@@ -0,0 +1,16 @@
+import requests
+KEY = "AIzaSyAGtU8i3hOlQXIuWL2t3eIBua9YXbC8x78"
+URL = "https://www.googleapis.com/civicinfo/v2/representatives"
+LEVELS = ["administrativeArea1", "administrativeArea2", "country", "international", "locality", "regional", "special",
+          "subLocality1", "subLocality2"]
+
+"""
+Returns 
+"""
+def fetch(address):
+    r = requests.get('%s?key=%s&address=%s' % (URL, KEY, address))
+    offices = r.json()['offices']
+    names = {lev: [] for lev in LEVELS}
+    for o in offices:
+        names[o['levels'][0]].append({'name': o['name'], 'division_id': o['divisionId']})
+    return names
---
+++ forward_api/forward_app/views/__init__.py
@@ -0,0 +1,2 @@
+from .main_views import *
+from .helper_views import *
