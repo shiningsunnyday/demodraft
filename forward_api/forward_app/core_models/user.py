d8339c99cc8184b0527d7e06ff8877df0affa627
Brian Li <bl195@duke.edu>
2020-08-04 20:39:41 -0400

    pulled and updated apidev

--- forward_api/forward_app/core_models/user.py
+++ forward_api/forward_app/core_models/user.py
@@ -1,21 +1,21 @@
-from django.db import models
 
-class User(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=True, default='')
-    email = models.CharField(max_length=100, blank=True, default='')
-    address = CharField(max_length=100, blank=True, default='')
-    class Meta:
-        ordering = ['created']
+from django.db import models
+from django.contrib.auth.models import User
 
 # """
 # One-to-one relation w/ User
 # """
 class Persona(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    users = models.ManyToManyField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, related_name="followers")
     created = models.DateTimeField(auto_now_add=True)
-    followers = models.IntegerField(default=0)
-    status = models.CharField(max_length=100, blank=True, default='')
+    num_followers = models.IntegerField(default=0)
+    stage = models.IntegerField(default=1)
+    line1 = models.CharField(max_length=200, blank=True, default='')
+    city = models.CharField(max_length=200, blank=True, default='')
+    state = models.CharField(max_length=20, blank=True, default='')
+    zipcode = models.IntegerField()
     class Meta:
         ordering = ['created']
+
+
