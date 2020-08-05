03c374fe9652e0402aa8d6792a9ada14cab03353
Jaytee <Padilla.Jaytee@gmail.com>
2020-08-05 13:46:35 -0700

    Moved code from CampaignPage into CampaignRegistration

--- forward_web/src/components/campaign/CampaignRegistration.vue
+++ forward_web/src/components/campaign/CampaignRegistration.vue
@@ -1,12 +1,55 @@
 <template>
   <div>
     <h1>CampaignRegistration</h1>
+
+    <b-container>
+      <b-form @submit.prevent="handleSubmit">
+        <b-form-group
+          id="address-group"
+          label="Address"
+          label-for="address"
+          description="(e.g. 1263 Pacific Ave. Kansas City, KS)"
+        >
+          <b-form-input id="address" v-model="address" type="text" required />
+        </b-form-group>
+        <b-button type="submit">Launch</b-button>
+      </b-form>
+    </b-container>
+    <pre>{{ civicData }}</pre>
   </div>
 </template>
 
 <script>
+import * as Config from "@/config.json";
+
 export default {
   name: "CampaignRegistration",
+  data() {
+    return {
+      civicData: undefined,
+      address: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      // move this to api-uils
+      this.$http({
+        url: `https://www.googleapis.com/civicinfo/v2/representatives`,
+        method: "GET",
+        params: {
+          address: this.address,
+          key: Config.GOOGLE_API_KEY,
+        },
+      })
+        .then((resp) => {
+          this.civicData = JSON.stringify(resp.data, null, "\t");
+        })
+        .catch((err) => {
+          alert("Please provide a valid address :-)");
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
 
--- forward_web/src/views/CampaignPage.vue
+++ forward_web/src/views/CampaignPage.vue
@@ -3,61 +3,20 @@
     <h1>CampaignPage</h1>
     <CampaignRegistration />
     <CampaignDetails />
-    <b-container>
-      <b-form @submit.prevent="handleSubmit">
-        <b-form-group
-          id="address-group"
-          label="Address"
-          label-for="address"
-        ></b-form-group>
-        <b-form-input
-          id="address"
-          v-model="address"
-          type="text"
-          required
-        ></b-form-input>
-        <b-button type="submit">Launch</b-button>
-      </b-form>
-    </b-container>
-    <pre>{{ civicData }}</pre>
   </div>
 </template>
 
 <script>
 import CampaignRegistration from "@/components/campaign/CampaignRegistration";
 import CampaignDetails from "@/components/campaign/CampaignDetails";
-import * as Config from '@/config.json';
+import * as Config from "@/config.json";
 
 export default {
   name: "CampaignPage",
   components: {
     CampaignRegistration,
-    CampaignDetails
+    CampaignDetails,
   },
-  data() {
-    return {
-      civicData: undefined,
-      address: '',
-    };
-  },
-  methods: {
-    handleSubmit() {
-      // move this to api-uils
-      this.$http({
-        url: `https://www.googleapis.com/civicinfo/v2/representatives`,
-        method: 'GET',
-        params: {
-          address: this.address,
-          key: Config.GOOGLE_API_KEY,
-        },
-      }).then(resp => {
-        this.civicData = JSON.stringify(resp.data,null,'\t');
-      }).catch(err => {
-        alert('Please provide a valid address :-)');
-        console.log(err);
-      });
-    }
-  }
 };
 </script>
 
