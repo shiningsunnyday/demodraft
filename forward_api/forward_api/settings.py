0de0a95e0db86045ecb0f605782c96d22c7e1df7
Brian Li <bl195@duke.edu>
2020-08-11 22:18:58 -0400

    changed settings

--- forward_api/forward_api/settings.py
+++ forward_api/forward_api/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'librian2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'librian1969'
+EMAIL_HOST_PASSWORD = '321Chicken!'
 
 #GMAIL API Configuration
 CLIENT_SECRET_FILE = 'client_secret.json'
