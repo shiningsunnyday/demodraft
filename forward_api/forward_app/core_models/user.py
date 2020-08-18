f4921cf777c5b9880032f3ae8aa11933dcffc035
Michael Sun <michaelsun18@yahoo.com>
2020-08-18 08:53:27 -0700

    Add Moderator Model template and score function doc

--- forward_api/forward_app/core_models/user.py
+++ forward_api/forward_app/core_models/user.py
@@ -1,21 +1,42 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 
-# """
-# One-to-one relation w/ User
-# """
+
+"""
+score function that decides moderator status
+    - needs calculating every time any of a Persona's data changes
+    - periodically, a sweeping operation happens through db that:
+        - updates score of every persona
+        - calculates top 10 percentile cutoff
+        - writes it to /utils/cutoff_history.txt
+        - promotes and degrades personas as needed
+"""
+def score(pers):
+    raise NotImplementedError
+
+
 class Persona(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     users = models.ManyToManyField(User, related_name="following")
     created = models.DateTimeField(auto_now_add=True)
     num_followers = models.IntegerField(default=0)
     stage = models.IntegerField(default=1)
+    score = models.FloatField(default=.0)
+
+    # need to refactor address
     line1 = models.CharField(max_length=200, blank=True, default='')
     city = models.CharField(max_length=200, blank=True, default='')
     state = models.CharField(max_length=20, blank=True, default='')
     zipcode = models.IntegerField(default=0)
+
     class Meta:
         ordering = ['created']
 
+
+class Moderator(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    persona = models.OneToOneField(Persona, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
 
