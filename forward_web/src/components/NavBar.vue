7841e1b687561afb8d04b3622c19f12a927ebfd8
Jaytee <Padilla.Jaytee@gmail.com>
2020-08-18 20:09:31 -0700

    Pushing current progress on Navbar styling

--- forward_web/src/components/NavBar.vue
+++ forward_web/src/components/NavBar.vue
@@ -1,45 +1,55 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand>
-      <router-link :to="{ name: 'about-page' }" class="navbar__link" v-if="!isLoggedIn">
-        About
-      </router-link>
-
-      <router-link :to="{ name: 'policies-page' }" class="navbar__link" v-else>
-        Policies
-      </router-link>
+    <b-navbar-brand class="font-weight-bold">
+      Demodraft
     </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav="">
-      <b-navbar-nav>
-        <!-- <b-nav-item v-if="isLoggedIn">
-          <router-link :to="{ name: 'about-page' }" class="navbar__link">
-            About
-          </router-link>
-        </b-nav-item> -->
-
-        <b-nav-item v-if="isLoggedIn">
-          <router-link :to="{ name: 'politician-page' }" class="navbar__link">
-            Politicians
-          </router-link>
+    <b-collapse id="nav-collapse" is-nav align="center">
+      <b-navbar-nav class="navbar__desktop-navbar">
+        <b-nav-item
+          :to="{ name: 'policies-page' }"
+          class="navbar__link"
+          v-if="isLoggedIn"
+        >
+          Policies
         </b-nav-item>
 
-        <b-nav-item v-if="isLoggedIn">
-          <router-link :to="{ name: 'campaign-page' }" class="navbar__link">
-            <span>Campaign</span>
-          </router-link>
+        <b-nav-item
+          :to="{ name: 'politician-page' }"
+          class="navbar__link"
+          v-if="isLoggedIn"
+        >
+          Politicians
         </b-nav-item>
 
-        <b-nav-item v-if="isLoggedIn" @click="logout">
+        <b-nav-item
+          :to="{ name: 'about-page' }"
+          class="navbar__link"
+          v-if="isLoggedIn"
+        >
+          About
+        </b-nav-item>
+
+        <b-nav-item
+          :to="{ name: 'campaign-page' }"
+          class="navbar__link"
+          v-if="isLoggedIn"
+        >
+          <span>Campaign</span>
+        </b-nav-item>
+
+        <b-nav-item class="navbar__link" v-if="isLoggedIn" @click="logout">
           Logout
         </b-nav-item>
 
-        <b-nav-item v-if="!isLoggedIn">
-          <router-link :to="{ name: 'login-page' }" class="navbar__link">
-            <span>Login</span>
-          </router-link>
+        <b-nav-item
+          :to="{ name: 'login-page' }"
+          class="navbar__link"
+          v-if="!isLoggedIn"
+        >
+          <span>Login</span>
         </b-nav-item>
       </b-navbar-nav>
     </b-collapse>
@@ -68,12 +78,23 @@ export default {
 .navbar {
   margin-bottom: 2rem;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  &__link {
-    color: white;
+  font-size: 1.2rem;
+  height: 100% !important;
 
-    &:hover {
-      color: greenyellow;
+  &__link {
+    padding: 0.5rem 2rem;
+
+    .nav-link {
+      color: white !important;
+
+      &:hover {
+        color: greenyellow !important;
+      }
     }
+  }
+
+  &__desktop-navbar {
+    margin: 0 auto;
   }
 }
 </style>
