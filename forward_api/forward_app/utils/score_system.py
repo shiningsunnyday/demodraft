be71a992b150c3c5527b4d8e58c266bd70799ae6
Michael Sun <michaelsun18@yahoo.com>
2020-08-20 09:55:18 -0700

    Add score field, add util functions in score_system and begin testing

--- forward_api/forward_app/utils/score_system.py
+++ forward_api/forward_app/utils/score_system.py
@@ -7,21 +7,30 @@ def line_prepender(filename, line):
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') + '\n' + content)
 
+"""
+Updates score of every Persona
+"""
+def update_scores(pers):
+    for p in pers:
+        p.score = p.num_followers / len(pers)
+
 
 """
-Computes cutoff from Persona QuerySet
+Computes cutoff from Persona QuerySet object
+Sorts all scores and gets 10th percentile
+Should later replace with Select O(n) algo
 """
-def cutoff(personas):
-    pass
+def cutoff(pers):
+    scores = sorted([p.score for p in pers])
+    return scores[int(9 * len(scores) / 10)]
 
 
-def sweep():
-    # cutoff = cutoff()
-    # for p in Persona.objects.all():
-    #     if p.score > cutoff and p.stage == 1:  # regular user that meets cutoff
-    #         p.stage = 2
-    line_prepender('forward_app/utils/cutoffs.txt', '%s,%f' % (str(datetime.date.today()), 1.0))
+def sweep(pers):
+    cut = cutoff(pers)
+    line_prepender('forward_app/utils/cutoffs.txt', '%s,%f' % (str(datetime.date.today()), cut))
+    for p in pers:
+        if p.score > cut and p.stage == 1:  # regular user that meets cutoff
+            p.stage = 2
 
 
-if __name__ == '__main__':
-    sweep()
+
