8e44c5a8eccc2a28b6706ee20122b8b31f89dd52
Michael Sun <michaelsun18@yahoo.com>
2020-08-28 18:07:39 -0700

    backed up db.sqlite3 and prepare pr

--- forward_api/forward_app/views/meta.py
+++ forward_api/forward_app/views/meta.py
@@ -4,6 +4,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticate
 
 
 class Meta(object):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     authentication_classes = [BasicAuthentication]
     parser_classes = [JSONParser]
