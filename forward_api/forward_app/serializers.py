d00af840f49d9dec46e3b9e4266cfd1c6da368d7
Jack <jdangrem@andrew.cmu.edu>
2020-09-03 23:21:23 -0600

    added token endpoint, updated user serializer for encrypted password; need to fix dependencies on plain-text passwords

--- forward_api/forward_app/serializers.py
+++ forward_api/forward_app/serializers.py
@@ -15,6 +15,16 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
 
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class PersonaSerializer(serializers.ModelSerializer):
